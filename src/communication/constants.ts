254166807c6f1f5544d521dccce309423be92949
David Boclé <bcldvd@gmail.com>
2017-09-24 14:29:49 +0200

    Change port to listen to env variable

--- src/communication/constants.ts
+++ src/communication/constants.ts
@@ -2,7 +2,7 @@
 export const HOST_WILLIAM = '127.0.0.1'// '10.1.38.61'
 export const HOST_DAVID = '192.168.1.97'
 export const HOST = WEBPACK_HOST || HOST_WILLIAM
-export const PORT = 3002
+export const PORT = process.env.PORT || 3002
 export const IO_SERVER = `http://${HOST}:${PORT}`
 
 export const MESSAGE_FROM_CLIENT = 'MESSAGE_FROM_CLIENT'
