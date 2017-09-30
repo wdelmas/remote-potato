7d6d7138552976f54e8b28e18a5de2fae44b5d23
David Boclé <bcldvd@gmail.com>
2017-09-30 17:08:20 +0200

    Remove zoom on mobile double tap

--- src/clients/mobiles/index.ts
+++ src/clients/mobiles/index.ts
@@ -37,6 +37,10 @@ const BASE_MESSAGE = {
     extensionId: ROOM_ID
 }
 
+
+removeDoubleTapZoom('button');
+
+
 export const play = () => {
     const message = Object.assign({}, {
         type: PLAYER_PLAY
@@ -95,4 +99,24 @@ export const exitFullScreen = () => {
         type: PLAYER_EXIT_FULLSCREEN,
     }, BASE_MESSAGE) as message
     socket.emit(MESSAGE_FROM_CLIENT, message);
+}
+
+function removeDoubleTapZoom(tagName: string) {
+    let tags = document.getElementsByTagName(tagName)
+    for (let i = tags.length; i--;) {
+        tags[i].addEventListener('touchstart', preventZoom)
+    }
+}
+
+function preventZoom(e: any) {
+    var t2 = e.timeStamp;
+    var t1 = e.currentTarget.dataset.lastTouch || t2;
+    var dt = t2 - t1;
+    var fingers = e.touches.length;
+    e.currentTarget.dataset.lastTouch = t2;
+
+    if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+    e.preventDefault();
+    e.target.click();
 }
