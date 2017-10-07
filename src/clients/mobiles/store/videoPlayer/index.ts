d9bdb28a2c543e66f62d73ba37534505cafdf7a0
William Delmas <william.delmas@gmail.com>
2017-10-07 23:38:13 +0200

    Dispach action when reciving new message from extension

--- src/clients/mobiles/store/videoPlayer/index.ts
+++ src/clients/mobiles/store/videoPlayer/index.ts
@@ -1,14 +1,24 @@
-import { PLAYER_PLAY } from "../../../../communication/actions";
+import { PLAYER_PLAY, VideoPlayerMessage } from "../../../../communication/actions";
+import { LOAD_CURRENT_VIDEO_STATE } from "./actions";
 
 export interface VideoPlayerReducer {
-    isPlaying: boolean
+    controller: {
+        isPlaying: boolean
+    },
+    current: VideoPlayerMessage
 }
 
 export default (state = emptyVideoPlayerReducer(), action: any) => {
     switch (action.type) {
+        case LOAD_CURRENT_VIDEO_STATE:
+            return Object.assign({}, state, {
+                current: action.current
+            })
         case PLAYER_PLAY:
             return Object.assign({}, state, {
-                isPlaying: state.isPlaying
+                controller: {
+                    isPlaying: action.isPlaying
+                }
             })
         default:
             return state
@@ -17,6 +27,9 @@ export default (state = emptyVideoPlayerReducer(), action: any) => {
 
 export function emptyVideoPlayerReducer(): VideoPlayerReducer {
     return {
-        isPlaying: false
+        controller: {
+            isPlaying: false
+        },
+        current: null
     }
 }
